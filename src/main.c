f32a6ad3d296ab81d5483436cc1f856e64e401a4
Vaishakh G K <vaishakh@vaishakhgk.com>
2026-09-18 14:13:17 +0530

    feat: Scan for WiFi networks
    - Scan for the WiFi networks through mentioned interface

--- src/main.c
+++ src/main.c
@@ -1,5 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    printf("WiFi Scanner");
+int main(int argc, char *argv[])
+{
+    if (argc != 2) {
+        printf("Usage: %s <interface>\n", argv[0]);
+        printf("Example: sudo %s wlan0\n", argv[0]);
+        return 1;
+    }
+
+    char command[256];
+
+    printf("WiFi Scanner\n");
+    printf("===========\n");
+    printf("Interface: %s\n\n", argv[1]);
+
+    snprintf(
+        command,
+        sizeof(command),
+        "iw dev %s scan | grep -E 'BSS |SSID:|signal:|freq:'",
+        argv[1]
+    );
+
+    system(command);
+
+    return 0;
 }
